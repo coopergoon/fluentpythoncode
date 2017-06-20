ea6401e58211c8b05759a069075d7aae01da5bd8
chen <roma1473@163.com>
2017-06-21 00:09:46 +0800

    01-data-model

--- 01-data-model/vector2d.py
+++ 01-data-model/vector2d.py
@@ -9,12 +9,45 @@ class Vector(object):
 		return 'Vetor(%r, %r)' %(self.x, self.y)
 
 	def __abs__(self):
-		return hypo(self.x, self.y)
+		"""
+		返回欧几里德范数 sqrt(x*x + y*y)。
+		"""
+		return hypot(self.x, self.y)
 
 	def __bool__(self):
 		return bool(abs(self))
 
-	def __add__(self):
-		x = self.x + other.x
-		y = self.y + other.y
+	def __add__(self, num):
+		"""
+		带参数形式
+		"""
+		x = self.x + num
+		y = self.y + num
+
+		# 不带参数形式
+		# x = self.x + other.x
+		# y = self.y + other.y
+
 		return Vector(x, y)
+
+	def __mul__(self, scalar):
+		"""
+		scalar只是形参
+		"""
+		return Vector(self.x*scalar, self.y*scalar)
+
+v = Vector(1, 2)
+# Vetor(1, 2)
+print(v)   # print创建的实例对象，输出的是在__repr__方法下的return内容
+
+# 2.23606797749979
+print(abs(v)) # 输出的是__abs__方法下的内容
+
+# True
+print(bool(v)) # 返回的是True或者False
+
+# Vetor(5, 6)
+print(v+4) # 调用__add__方法，只需使用+，做加法运算
+
+# Vetor(10, 20)
+print(v*10) # 调用后__mul__方法，只需使用*,做乘法运算 
